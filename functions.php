2a910138cf53a8b05ce23e2d83c171b00dbccae2
Bas Middelham <bas@middelham.nl>
2020-05-20 13:17:43 +0200

    Fix widgets

--- functions.php
+++ functions.php
@@ -77,8 +77,8 @@ function strt_widgets_init() {
 			'description'   => esc_html__( 'Add widgets here.', 'strt' ),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
 		)
 	);
 }
--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
-<form role="search" method="get" class="search-form input-group" action="<?php echo home_url('/'); ?>">
-  <input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder') ?>"
+<form role="search" method="get" class="search-form input-group mw-75" action="<?php echo home_url('/'); ?>">
+  <input type="search" class="search-field form-control form-control-sm" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder') ?>"
       value="<?php echo get_search_query() ?>" name="s"
       title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
   <div class="input-group-append">
-    <input type="submit" class="search-submit btn btn-primary" value="<?php echo esc_attr_x('Search', 'submit button') ?>" />
+    <input type="submit" class="search-submit btn btn-primary btn-sm" value="<?php echo esc_attr_x('Search', 'submit button') ?>" />
   </div>
 </form>
